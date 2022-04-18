a3b3ab1e2d5ad0505449cd60f2e778abe6f659c3
GuntaXD <juanferreira65@gmail.com>
2022-04-18 13:36:27 -0400

    buscador de canciones

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { getSelectionRange } from '@testing-library/user-event/dist/utils';
 import React from 'react';
 import CrudApi from './components/CrudApi';
 import CrudApp from './components/CrudApp';
+import SongSearch from './components/SongSearch';
 
 function App() {
   return (
     <>
       <h1>Ejercicios con react</h1>
+      <hr/>
+      <SongSearch/>
       <hr/>
       <CrudApi/>
       <hr/>
--- src/components/Message.js
+++ src/components/Message.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 export const Message = ({msg , bgColor }) => {
     let style = {
+        height: "5rem",
         padding: "1rem",
         marginBottom: "1rem",
         textAlign: "center",
